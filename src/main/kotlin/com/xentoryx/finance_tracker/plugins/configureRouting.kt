eab0f3a4aa49cf41e6dad34e1aca95be70b8896d
Mohammad Asiful Islam <142866027+mohammadasifulislam8899@users.noreply.github.com>
2026-05-13 10:40:47 +0600

    Routes Added

--- src/main/kotlin/com/xentoryx/finance_tracker/plugins/configureRouting.kt
+++ src/main/kotlin/com/xentoryx/finance_tracker/plugins/configureRouting.kt
@@ -1,7 +1,10 @@
 ﻿package com.xentoryx.finance_tracker.plugins
 
+import com.xentoryx.finance_tracker.presentation.routes.accountRoutes
 import com.xentoryx.finance_tracker.presentation.routes.authRoutes
+import com.xentoryx.finance_tracker.presentation.routes.categoryRoutes
 import com.xentoryx.finance_tracker.presentation.routes.healthRoutes
+import com.xentoryx.finance_tracker.presentation.routes.transactionRoutes
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
 
@@ -10,5 +13,8 @@ fun Application.configureRouting() {
     routing {
         healthRoutes()
         authRoutes()
+        accountRoutes()
+        categoryRoutes()
+        transactionRoutes()
     }
 }
